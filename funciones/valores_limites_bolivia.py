b82ec8d750527e392bc3d89d11bc01b13e831d06
Sylvain Lesage <severo@rednegra.net>
2014-05-31 22:48:01 -0400

    Utilizar los mismos cálculos que el INE

--- funciones/valores_limites_bolivia.py
+++ funciones/valores_limites_bolivia.py
@@ -11,17 +11,18 @@ LIMITES_BOLIVIA_PATH = os.path.join(DATA_PATH, 'limite_nacional.gml')
 
 # Abrir el archivo
 ds = ogr.Open(LIMITES_BOLIVIA_PATH)
+print "Archivo:"
 print ds.GetName()
-print ds.GetLayerCount()
 
 # Recuperar la primera y unica capa
 layer = ds.GetLayer(0)
+print "Capa: "
 print layer.GetName()
-print layer.GetFeatureCount()
 origSpatialRef = layer.GetSpatialRef()
 
-# Recuperar el unico "feature"
+# Recuperar la geometria del unico "feature"
 feature = layer.GetFeature(1)
+geometry = feature.GetGeometryRef()
 
 # Definir la proyeccion conica conforme de Lambert de Bolivia
 lambert = osr.SpatialReference()
@@ -29,34 +30,52 @@ lambert.ImportFromProj4("+proj=lcc +lat_1=-11.5 +lat_2=-21.5 +lat_0=-24 +lon_0=-
 
 # Reproyectar en Lambert
 SpatialRefTransform = osr.CoordinateTransformation(origSpatialRef,lambert)
-geometry = feature.GetGeometryRef()
-#print geometry.Length()
 geometry.Transform(SpatialRefTransform)
-#print geometry.Length()
 
 # Calcular r y tita para cada punto
 pts = geometry.GetGeometryRef(0)
-points = []
-x = []
-y = []
-r = []
-tita = []
+vx = []
+vy = []
+vr = []
+vtita = []
+vz = []
 for p in xrange(pts.GetPointCount()):
-	points.append((pts.GetX(p), pts.GetY(p)))
-	x.append(pts.GetX(p))
-	y.append(pts.GetY(p))
-	points.append((pts.GetX(p), pts.GetY(p)))
-	r.append(math.sqrt(pts.GetX(p)**2+ pts.GetY(p)**2))
-	tita.append(np.arctan(pts.GetY(p)/pts.GetX(p)))
+	x = pts.GetX(p)
+	y = pts.GetY(p)
+	r = math.sqrt(x**2+ y**2)
+	tita = np.arctan(y/x)+np.pi
+	z = math.floor(r * math.exp(tita))
+	
+	vx.append(x)
+	vy.append(y)
+	vr.append(r)
+	vtita.append(tita)
+	vz.append(z)
 
 # Min y Max
 
-print min(r)
-print max(r)
-print min(r)
-print max(r)
-print min(tita)
-print max(tita)
+print "Valores de x"
+print min(vx)
+print max(vx)
+print "Valores de y"
+print min(vy)
+print max(vy)
+print "Valores de r"
+print min(vr)
+print max(vr)
+print "Valores de tita"
+print min(vtita)
+print max(vtita)
+print "Valores de z"
+print min(vz)
+print max(vz)
+#print math.floor(min(vr) * math.exp(min(vtita)))
+#print math.floor(max(vr) * math.exp(max(vtita)))
 
-print math.floor(min(r) * math.exp(min(tita)))
-print math.floor(max(r) * math.exp(max(tita)))
+# Numero de codigos
+print "Numero de codigos"
+print max(vz) - min(vz)
+
+# Superficie de Bolivia
+area_bolivia = geometry.ConvexHull().GetArea()
+print "Superficie Bolivia (m2) = %d" % area_bolivia
