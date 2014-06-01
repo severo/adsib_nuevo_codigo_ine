fc7e3260adfc04116bc9090f252a4ac700d1a292
Sylvain Lesage <severo@rednegra.net>
2014-06-01 15:03:24 -0400

    Resultados para varias capitales departamentales

--- funciones/valores_limites_bolivia.py
+++ funciones/valores_limites_bolivia.py
@@ -22,6 +22,73 @@ def calcular_codigo(lon,lat):
 
 	return (r,tita,z)
 
+# Calcula la franja que pasa por un punto
+def calcular_franja(lon,lat,nombre,poly_bolivia):
+	(r,tita,z) = calcular_codigo(lon,lat)
+
+	## Generar un SHP con esta franja
+	drv = ogr.GetDriverByName("ESRI Shapefile")
+	outputfile = "../resultados/" + nombre + ".shp"
+	if os.path.exists(outputfile):
+		drv.DeleteDataSource(outputfile)
+	output = drv.CreateDataSource(outputfile)
+	franja = output.CreateLayer("Franja", geom_type=ogr.wkbPolygon)
+
+	lambert = osr.SpatialReference()
+	lambert.ImportFromProj4("+proj=lcc +lat_1=-11.5 +lat_2=-21.5 +lat_0=-24 +lon_0=-64 +x_0=1000000 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
+	wgs84 = osr.SpatialReference()
+	wgs84.ImportFromEPSG(4326)
+	transfWGS84 = osr.CoordinateTransformation(lambert,wgs84)
+
+	## Calcular la franja de territorio con el mismo codigo que el punto
+	mintita = min(vtita)
+	maxtita = max(vtita)
+	paso_tita = (maxtita-mintita)/1000
+	vrmin = []
+	vptmin = []
+	vrmax = []
+	vptmax = []
+	vdeltar = []
+	for i in range(0,999):
+		tita = mintita + i*paso_tita
+		rmin = z * math.exp(- tita)
+		rmax = (z+1) * math.exp(- tita)
+		vrmin.append(rmin)
+		vptmin.append((rmin,tita))
+		vrmax.append(rmax)
+		vptmax.append((rmax,tita))
+		vdeltar.append(rmax-rmin)
+
+	vpt = vptmin
+	vpt.extend(reversed(vptmax))
+
+	ring = ogr.Geometry(ogr.wkbLinearRing)
+	for i in vpt:
+		x_franja = i[0]*math.cos(i[1]-math.pi)
+		y_franja = i[0]*math.sin(i[1]-math.pi)
+		ring.AddPoint(x_franja,y_franja)
+
+	ring.CloseRings()
+
+	poly = ogr.Geometry(ogr.wkbPolygon)
+	poly.AddGeometry(ring)
+
+	poly_intersection = poly.Intersection(poly_bolivia)
+
+	print "************************************************"
+	print "La franja que pasa por " + nombre + ":"
+	print " * tiene una superficie de %dm2 (equivalente de un cuadrado de %dm de lado)" % (poly_intersection.GetArea(),math.sqrt(poly_intersection.GetArea()))
+	print " * es un 'arco' de %dkm de largo" % (poly_intersection.Boundary().Length()/2000)
+	print "                 y %.2fcm de ancho (promedio - min=%.2fcm max=%.2fcm)" % (float( sum(vdeltar) ) / len(vdeltar)*100,min(vdeltar)*100,max(vdeltar)*100)
+
+	poly_intersection.Transform(transfWGS84)
+	f = ogr.Feature(feature_def=franja.GetLayerDefn())
+	f.SetGeometryDirectly(poly_intersection)
+	franja.CreateFeature(f)
+
+	output.Destroy()
+
+
 #DATA_PATH = os.path.dirname("../datos/")
 #LIMITES_BOLIVIA_PATH = os.path.join(DATA_PATH, 'limite_nacional.gml')
 DATA_PATH = os.path.dirname("../datos/BOL_adm/")
@@ -97,67 +164,8 @@ print "Superficie promedio por codigo (m2) = %d" % area_por_codigo
 print "Lado de un cuadrado de la superficie promedio por codigo (m) = %d" % lado_area_por_codigo
 
 # Tests con La Paz, plaza Murillo
-lon_LP = -68.13352
-lat_LP = -16.49568
-(r_LP,tita_LP,z_LP) = calcular_codigo(lon_LP,lat_LP)
-print "La Paz, plaza Murillo (lat: %f, lon: %f) - r=%f, tita=%f, z=%d" % (lat_LP,lon_LP,r_LP,tita_LP,z_LP)
-
-## Generar un SHP con esta franja
-drv = ogr.GetDriverByName("ESRI Shapefile")
-outputfile = "../resultados/franja.shp"
-if os.path.exists(outputfile):
-	drv.DeleteDataSource(outputfile)
-output = drv.CreateDataSource(outputfile)
-franja = output.CreateLayer("Franja", geom_type=ogr.wkbPolygon)
-
-lambert = osr.SpatialReference()
-lambert.ImportFromProj4("+proj=lcc +lat_1=-11.5 +lat_2=-21.5 +lat_0=-24 +lon_0=-64 +x_0=1000000 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
-wgs84 = osr.SpatialReference()
-wgs84.ImportFromEPSG(4326)
-transfWGS84 = osr.CoordinateTransformation(lambert,wgs84)
-
-## Calcular la franja de territorio con el mismo codigo que La Paz
-mintita = min(vtita)
-maxtita = max(vtita)
-paso_tita = (maxtita-mintita)/1000
-vrmin = []
-vptmin = []
-vrmax = []
-vptmax = []
-vdeltar = []
-for i in range(0,999):
-	tita = mintita + i*paso_tita
-	rmin = z_LP * math.exp(- tita)
-	rmax = (z_LP+1) * math.exp(- tita)
-	vrmin.append(rmin)
-	vptmin.append((rmin,tita))
-	vrmax.append(rmax)
-	vptmax.append((rmax,tita))
-	vdeltar.append(rmax-rmin)
-
-vpt = vptmin
-vpt.extend(reversed(vptmax))
-
-ring = ogr.Geometry(ogr.wkbLinearRing)
-for i in vpt:
-	x_franja = i[0]*math.cos(i[1]-math.pi)
-	y_franja = i[0]*math.sin(i[1]-math.pi)
-	ring.AddPoint(x_franja,y_franja)
-
-ring.CloseRings()
-
-poly = ogr.Geometry(ogr.wkbPolygon)
-poly.AddGeometry(ring)
-poly_intersection = poly.Intersection(poly_bolivia)
-
-print "La franja que pasa por la Paz:"
-print " * tiene una superficie de %dm2" % poly_intersection.GetArea()
-print " * es un arco de %dkm de largo" % (poly_intersection.Boundary().Length()/2000)
-print "               y %.2fcm de ancho (promedio - min=%.2fcm max=%.2fcm)" % (float( sum(vdeltar) ) / len(vdeltar)*100,min(vdeltar)*100,max(vdeltar)*100)
-
-poly_intersection.Transform(transfWGS84)
-f = ogr.Feature(feature_def=franja.GetLayerDefn())
-f.SetGeometryDirectly(poly_intersection)
-franja.CreateFeature(f)
-
-output.Destroy()
+calcular_franja(-68.13352,-16.49568,"La_Paz_plaza_murillo",poly_bolivia)
+calcular_franja(-63.18213,-17.78339,"Santa_Cruz_plaza_24_de_septiembre",poly_bolivia)
+calcular_franja(-66.15696,-17.39377,"Cochabamba_plaza_14_de_septiembre",poly_bolivia)
+calcular_franja(-68.75364,-11.01827,"Cobija_plaza_german_busch",poly_bolivia)
+calcular_franja(-64.73440,-21.53390,"Tarija_plaza_luis_de_fuentes",poly_bolivia)
